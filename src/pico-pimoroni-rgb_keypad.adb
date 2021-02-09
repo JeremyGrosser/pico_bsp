d4b5e37f0a6ecb6c0766048dce0b569f80cc58c1
Fabien Chouteau <chouteau@adacore.com>
2021-02-09 12:45:08 +0100

    Add drivers for the Pimoroni RGB Keypad

---
+++ src/pico-pimoroni-rgb_keypad.adb
@@ -0,0 +1,146 @@
+with HAL;      use HAL;
+with HAL.SPI;  use HAL.SPI;
+with HAL.GPIO; use HAL.GPIO;
+
+with RP.SPI;
+with RP.Device;
+
+package body Pico.Pimoroni.RGB_Keypad is
+
+   SPI : RP.SPI.SPI_Port renames RP.Device.SPI_0;
+   SPI_CLK : RP.GPIO.GPIO_Point renames Pico.GP18;
+   SPI_MOSI : RP.GPIO.GPIO_Point renames Pico.GP19;
+   SPI_CS : RP.GPIO.GPIO_Point renames Pico.GP17;
+
+   Nbr_Of_LEDs : constant := 16;
+
+   LED_Data : SPI_Data_8b (1 .. 4 + Nbr_Of_LEDs * 4 + 4) :=
+     (0, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      2#111_00000#, 0, 0, 0,
+      0, 0, 0, 0
+     );
+
+   ----------------
+   -- Initialize --
+   ----------------
+
+   procedure Initialize is
+   begin
+      RP.Clock.Enable (RP.Clock.PERI);
+
+      SPI.Set_Speed (1_000_000);
+      SPI.Enable;
+
+      SPI_CS.Configure (Output);
+      SPI_CS.Set;
+
+      SPI_CLK.Configure (Output, Floating, RP.GPIO.SPI);
+      SPI_MOSI.Configure (Output, Floating, RP.GPIO.SPI);
+   end Initialize;
+
+   ---------
+   -- Set --
+   ---------
+
+   procedure Set (P          : Pad;
+                  R, G, B    : HAL.UInt8;
+                  Brightness : HAL.UInt5 := HAL.UInt5'Last)
+   is
+      Index : constant Natural := LED_Data'First + 4 + Natural (P) * 4;
+   begin
+
+      LED_Data (Index) := 2#111_00000# or UInt8 (Brightness);
+      LED_Data (Index + 1) := B;
+      LED_Data (Index + 2) := G;
+      LED_Data (Index + 3) := R;
+   end Set;
+
+   -------------
+   -- Set_HSV --
+   -------------
+
+   procedure Set_HSV (P          : Pad;
+                      H, S, V    : HAL.UInt8;
+                      Brightness : HAL.UInt5 := HAL.UInt5'Last)
+   is
+      R, G, B : UInt8;
+
+      Region, Remainder : UInt32;
+      A, Q, T : UInt8;
+   begin
+      if S = 0 then
+         R := V;
+         G := V;
+         B := V;
+      else
+
+         Region := UInt32 (H / 43);
+         Remainder := (UInt32 (H) - (Region * 43)) * 6;
+
+         A := UInt8 (Shift_Right (UInt32 (V) * (255 - UInt32 (S)), 8));
+
+         Q := UInt8 (Shift_Right (UInt32 (V) *
+                     (255 - Shift_Right (UInt32 (S) * Remainder, 8)), 8));
+
+         T := UInt8 (Shift_Right (UInt32 (V) *
+                     (255 - Shift_Right (UInt32 (S) *
+                        (255 - Remainder), 8)), 8));
+
+         case (Region) is
+            when      0 => R := V; G := T; B := A;
+            when      1 => R := Q; G := V; B := A;
+            when      2 => R := A; G := V; B := T;
+            when      3 => R := A; G := Q; B := V;
+            when      4 => R := T; G := A; B := V;
+            when others => R := V; G := A; B := Q;
+         end case;
+      end if;
+
+      Set (P, R, G, B, Brightness);
+   end Set_HSV;
+
+   ---------
+   -- Get --
+   ---------
+
+   procedure Get (P          :     Pad;
+                  R, G, B    : out HAL.UInt8;
+                  Brightness : out HAL.UInt5)
+   is
+      Index : constant Natural := LED_Data'First + 4 + Natural (P) * 4;
+   begin
+
+      Brightness := UInt5 (LED_Data (Index) and 2#000_11111#);
+      B := LED_Data (Index + 1);
+      G := LED_Data (Index + 2);
+      R := LED_Data (Index + 3);
+   end Get;
+
+   ------------
+   -- Update --
+   ------------
+
+   procedure Update is
+      Status : SPI_Status;
+   begin
+      SPI_CS.Clear;
+      SPI.Transmit (LED_Data, Status);
+      SPI_CS.Set;
+   end Update;
+
+end Pico.Pimoroni.RGB_Keypad;

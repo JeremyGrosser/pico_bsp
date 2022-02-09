963fe1528e483516685fecb75a464f3528c3c093
Jeremy Grosser <jeremy@synack.me>
2022-02-09 14:51:42 -0800

    Pico.Audio_I2S is deprecated, use RP.PIO.Audio_I2S instead

--- src/pico-audio_i2s.ads
+++ src/pico-audio_i2s.ads
@@ -1,2 +1,3 @@
+--  Pico.Audio_I2S is deprecated, use RP.PIO.Audio_I2S instead
 with RP.PIO.Audio_I2S;
 package Pico.Audio_I2S renames RP.PIO.Audio_I2S;
--- src/pico-pimoroni-audio_pack.ads
+++ src/pico-pimoroni-audio_pack.ads
@@ -3,13 +3,13 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
+with RP.PIO.Audio_I2S;
 with RP.Device;
 with RP.GPIO;
-with Pico.Audio_I2S;
 
 package Pico.Pimoroni.Audio_Pack is
 
-   I2S  : Pico.Audio_I2S.I2S_Device
+   I2S  : RP.PIO.Audio_I2S.I2S_Device
       (Data        => Pico.GP9'Access,
        BCLK        => Pico.GP10'Access,
        LRCLK       => Pico.GP11'Access,

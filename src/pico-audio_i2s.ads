28d9437c51eb0d2d5a0b0e9715ab4d943f33a373
Jeremy Grosser <jeremy@synack.me>
2022-04-02 10:35:27 -0700

    Release 1.4.0

--- src/pico-audio_i2s.ads
+++ src/pico-audio_i2s.ads
@@ -1,3 +1,8 @@
+--
+--  Copyright (C) 2022 Jeremy Grosser <jeremy@synack.me>
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
 --  Pico.Audio_I2S is deprecated, use RP.PIO.Audio_I2S instead
 with RP.PIO.Audio_I2S;
 package Pico.Audio_I2S renames RP.PIO.Audio_I2S;
